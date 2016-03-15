9b0c9175cae8d486c3d4e1e83540175a9e6ee441
trikitrok <trikitrok@gmail.com>
2016-03-15 14:55:32 +0100

    Using the operator as key in operation

--- src/cljs/math_ops/game.cljs
+++ src/cljs/math_ops/game.cljs
@@ -10,8 +10,9 @@
 (defn make-operation []
   (let [op1 (random-0-9)
         op2 (random-0-9)
-        res (* op1 op2)]
-    (hide-sth (->Operation op1 :* op2 res))))
+        operator *
+        res (operator op1 op2)]
+    (hide-sth (->Operation op1 operator op2 res))))
 
 (defn find-unknown [operation]
   (ffirst
@@ -21,12 +22,8 @@
 (defn substitute-unknown [operation guess]
   (assoc operation (find-unknown operation) guess))
 
-(defn apply-operator [operator & ops]
-  (let [operators {:* *}]
-    (apply (operators operator) ops)))
-
 (defn correct? [{:keys [res op1 op2 operator]}]
-  (= res (apply-operator operator op1 op2)))
+  (= res (operator op1 op2)))
 
 (defn correct-guess? [operation guess]
   (correct? (substitute-unknown operation guess)))
