cdb5b21efc58080951109fbbba1837138196060b
francesc <francesc.guillen@gmail.com>
2016-03-16 12:12:26 +0100

    input inline without html inputbox

--- src/cljs/math_ops/views.cljs
+++ src/cljs/math_ops/views.cljs
@@ -1,34 +1,44 @@
 (ns math-ops.views
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]))
 
 (def string-by-keyword {* "x" + "+" - "-" / "/" :? "?"})
 
-(defn pprint-keyword [s]
-  (get string-by-keyword s s))
-
-(defn display [{:keys [op1 op2 operator res]}]
-  [:div (clojure.string/join " " (map pprint-keyword [op1 operator op2 "=" res]))])
-
 (defn check [guess-input]
   (re-frame/dispatch [:check-guess (int @guess-input)])
-  (reset! guess-input ""))
+  (reset! guess-input "?"))
 
-(defn atom-input [value]
-  [:input {:type :text
-           :size 4
-           :value @value
-           :on-change #(reset! value (-> % .-target .-value))}])
+(defn add-char [s c]
+  (apply str (remove #(= % "?") (str s c)))
+  )
 
-(defn guessing-component []
-  (let [guess (reagent.ratom/atom "")]
+(defn process-key [input-atom key-code]
+  (if (= 13 key-code)
+    (check input-atom)
+    (let [c (char key-code)]
+     (swap! input-atom add-char c)))
+  )
+
+(defn unknow-component [display-value]
+  (let [input (reagent.ratom/atom display-value)]
     (fn []
-      [:div
-       [:div "Guess: " (atom-input guess)]
-       [:div [:button {:on-click #(check guess)} "Check"]]])))
+      [:span.unknown {:style {:margin "5px"}
+                      :tab-index 1
+                      :on-key-press #(process-key input (.-charCode  %1))} @input])))
+
+(defn pprint-keyword [s]
+  (let [display-value (get string-by-keyword s s)]
+    (if (= :? s)
+      [unknow-component display-value]
+      [:span.known {:style {:margin "5px"}} display-value])))
+
+(defn display [{:keys [op1 op2 operator res]}]
+  [:div (map pprint-keyword [op1 operator op2 "=" res])])
+
 
 (defn main-panel []
   (let [operation (re-frame/subscribe [:operation])]
     (fn []
       [:div
        (display @operation)
-       [guessing-component]])))
+       ])))
