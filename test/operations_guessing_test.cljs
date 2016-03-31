1963347b36cbff05c0638929ce9086774d840839
francesc <francesc.guillen@gmail.com>
2016-03-31 15:00:36 +0200

    making test robust against app state changes

--- test/operations_guessing_test.cljs
+++ test/operations_guessing_test.cljs
@@ -1,45 +1,48 @@
 (ns math-ops.operations-guessing-test
   (:require
-    [cljs.test :refer-macros [deftest is testing]]
-    [math-ops.operations :as operations]
-    [math-ops.operations-guessing :refer [start process-input]]))
+   [cljs.test :refer-macros [deftest is testing]]
+   [math-ops.operations :as operations]
+   [math-ops.operations-guessing :refer [start process-input]]))
 
 (def new-operation (operations/->Operation 15 + 3 :?))
 
 (deftest operation-guessing
 
   (with-redefs
-    [operations/make (constantly new-operation)]
+   [operations/make (constantly new-operation)]
 
     (testing start
       (testing operationg-guessing-start
-        (is (= (start) {:operation new-operation :number-input "?"}))))
+        (let [new-state (start)]
+          (is (= (:operation new-state) new-operation))
+          (is (= (:number-input new-state) "?")))))
 
     (testing processing-user-input
       (testing when-not-a-number-is-pressed-the-state-remains-unaltered
         (let [arbitrary-state {:operation :not-used-in-this-test :number-input "?"}
-              key-code-for-some-arbitrary-not-number 103]
-          (is (= (process-input arbitrary-state key-code-for-some-arbitrary-not-number)
-                 arbitrary-state))))
+              key-code-for-some-arbitrary-not-number 103
+              new-state (process-input arbitrary-state key-code-for-some-arbitrary-not-number)]
+          (is (= new-state arbitrary-state))))
 
       (testing when-a-number-is-pressed-the-number-gets-added-to-the-number-input
         (let [key-code-for-3 51]
-          (is (= (process-input {:operation :not-used-in-this-test :number-input "?"} key-code-for-3)
-                 {:operation :not-used-in-this-test :number-input "3"}))
-
-          (is (= (process-input {:operation :not-used-in-this-test :number-input "56"} key-code-for-3)
-                 {:operation :not-used-in-this-test :number-input "563"}))))
+          (let [new-state (process-input {:operation :not-used-in-this-test :number-input "?"} key-code-for-3)]
+            (is (= (:number-input new-state) "3")))
+          (let [new-state (process-input {:operation :not-used-in-this-test :number-input "56"} key-code-for-3)]
+            (is (= (:number-input new-state) "563")))))
 
       (testing when-enter-is-pressed-and-the-guess-is-wrong-the-guessing-restarts-keeping-the-same-operation
         (let [key-code-for-enter 13
               arbitrary-operation (operations/->Operation 14 - :? 9)
-              wrong-guess "8"]
-          (is (= (process-input {:operation arbitrary-operation :number-input wrong-guess} key-code-for-enter)
-                 {:operation arbitrary-operation :number-input "?"}))))
+              wrong-guess "8"
+              new-state (process-input {:operation arbitrary-operation :number-input wrong-guess} key-code-for-enter)]
+          (is (= (:operation new-state) arbitrary-operation))
+          (is (= (:number-input new-state) "?"))))
 
       (testing when-enter-is-pressed-and-the-guess-is-right-the-guessing-restarts-with-a-new-operation
         (let [key-code-for-enter 13
               arbitrary-operation (operations/->Operation 8 + :? 9)
-              right-guess "1"]
-          (is (= (process-input {:operation arbitrary-operation :number-input right-guess} key-code-for-enter)
-                 {:operation new-operation :number-input "?"})))))))
+              right-guess "1"
+              new-state (process-input {:operation arbitrary-operation :number-input right-guess} key-code-for-enter)]
+          (is (= (:operation new-state) new-operation))
+          (is (= (:number-input new-state) "?")))))))
