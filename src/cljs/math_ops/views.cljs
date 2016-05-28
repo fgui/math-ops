0729b0bd9f8a6c7bb3beba36a4d62182df761b4b
francesc <francesc.guillen@gmail.com>
2016-05-28 12:52:56 +0200

    pretty print history: color correct, bold answer

--- src/cljs/math_ops/views.cljs
+++ src/cljs/math_ops/views.cljs
@@ -2,7 +2,7 @@
   (:require
     [math-ops.game-levels :as game-levels]
     [re-frame.core :as re-frame]
-    [math-ops.operations :refer [symbols-description]]))
+    [math-ops.operations :refer [symbols-description correct-guess?]]))
 
 (defn unknow-component []
   (let [number-input (re-frame/subscribe [:number-input])]
@@ -27,32 +27,45 @@
 (defn display-known-symbol [k display-value]
   ^{:key k} [:span.known {:style {:margin "5px"}} display-value])
 
-(defn display-unknown-symbol []
+(defn display-unknown-symbol [k]
   ^{:key :unknown-symbol} [unknow-component])
 
-(defn display-symbol [k s]
+(defn make-display-answer-symbol [answer]
+  (fn [k]
+    ^{:key k} [:span.known {:style {:margin "5px"
+                                    :font-weight :bold}} answer]))
+
+(defn display-symbol [k s f-display-unknown-symbol]
   (let [display-value (get symbols-description s s)]
     (if (= :? s)
-      (display-unknown-symbol)
+      (f-display-unknown-symbol k)
       (display-known-symbol k display-value))))
 
-(defn display-operation [{:keys [op1 op2 operator res]}]
+(defn display-operation [{:keys [op1 op2 operator res]}
+                         f-display-unknown-symbol]
   [:div
-   (map display-symbol
+   (map #(display-symbol %1 %2 f-display-unknown-symbol)
         [:op1 :operator :op2 :equal-symbol :res]
         [op1 operator op2 "=" res])])
+
+(defn display-guessing-result [{:keys [operation number-input]}]
+  (let [correct? (correct-guess? operation number-input)]
+    ;; TODO color with css
+    [:div {:style {:color (if correct? :green :red)}}
+     (display-operation operation
+                        (make-display-answer-symbol number-input))]))
 
 (defn history-display-component []
   (let [history (re-frame/subscribe [:history])]
     (fn []
       (println "history")
-      [:div (map #(display-operation (:operation %)) @history)])))
+      [:div (map display-guessing-result @history)])))
 
 (defn main-panel []
   (let [operation (re-frame/subscribe [:operation])]
     (fn []
       [:div
-       (display-operation @operation)
+       (display-operation @operation display-unknown-symbol)
        [level-component]
        [level-selection-component]
        [history-display-component]])))
