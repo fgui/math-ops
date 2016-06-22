b8d7aa20195587c63e4742cd4047b44e7f4a6556
trikitrok <trikitrok@gmail.com>
2016-06-22 15:23:16 +0200

    only storing state when the pressed key was enter

--- src/cljs/math_ops/handlers.cljs
+++ src/cljs/math_ops/handlers.cljs
@@ -1,9 +1,9 @@
 (ns math-ops.handlers
   (:require
-   [re-frame.core :as re-frame]
-   [math-ops.operations-guessing :as operations-guessing]
-   [math-ops.history :as history]
-   [hodgepodge.core :refer [local-storage]]))
+    [re-frame.core :as re-frame]
+    [math-ops.operations-guessing :as operations-guessing]
+    [math-ops.history :as history]
+    [hodgepodge.core :refer [local-storage]]))
 
 (defn state->storage! [state]
   (assoc! local-storage :math-ops-state state))
@@ -11,35 +11,47 @@
 (defn storage->state []
   (get local-storage :math-ops-state))
 
+(defn- return-pressed? [key-code]
+  (= 13 key-code))
+
+(defmulti should-store?
+          (fn [[event-type _]]
+            event-type))
+
+(defmethod should-store? :press-key [[_ key-code]]
+  (return-pressed? key-code))
+
+(defmethod should-store? :default [_]
+  true)
+
 (defn store-middleware [handler]
-  (fn [state b]
-    (let [new-state (handler state b)]
-      (print "in middleware")
-      (state->storage! new-state)
-      new-state)
-    ))
+  (fn [state event]
+    (let [new-state (handler state event)]
+      (when (should-store? event)
+        (state->storage! new-state))
+      new-state)))
 
 (re-frame/register-handler
- :initialize-state
- (fn [_ _]
-   (merge {:current-level :max-level
-           :guessing (operations-guessing/start-new-guessing :max-level)
-           :history []}
-          (storage->state))))
+  :initialize-state
+  (fn [_ _]
+    (merge {:current-level :max-level
+            :guessing (operations-guessing/start-new-guessing :max-level)
+            :history []}
+           (storage->state))))
 
 (re-frame/register-handler
- :press-key
- store-middleware
- (fn [{:keys [history guessing current-level] :as state} [_ key-code]]
-   (let [new-guessing (operations-guessing/process-input guessing current-level key-code)]
-     (-> state
-         (assoc :history (history/record guessing new-guessing history))
-         (assoc :guessing new-guessing)))))
+  :press-key
+  store-middleware
+  (fn [{:keys [history guessing current-level] :as state} [_ key-code]]
+    (let [new-guessing (operations-guessing/process-input guessing current-level key-code)]
+      (-> state
+          (assoc :history (history/record guessing new-guessing history))
+          (assoc :guessing new-guessing)))))
 
 (re-frame/register-handler
- :select-level
- store-middleware
- (fn [state [_ key-level]]
-   (-> state
-       (assoc :current-level key-level)
-       (assoc :guessing (operations-guessing/start-new-guessing key-level)))))
+  :select-level
+  store-middleware
+  (fn [state [_ key-level]]
+    (-> state
+        (assoc :current-level key-level)
+        (assoc :guessing (operations-guessing/start-new-guessing key-level)))))
