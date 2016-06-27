02d389fec7c3e956e971506b20f856fa6039e3f5
trikitrok <trikitrok@gmail.com>
2016-06-27 15:16:43 +0200

    also work in Firefox

--- src/cljs/math_ops/core.cljs
+++ src/cljs/math_ops/core.cljs
@@ -5,20 +5,23 @@
     [math-ops.handlers]
     [math-ops.subs]
     [math-ops.views :as views]
-    [math-ops.config :as config]))
+    [math-ops.config :as config]
+    [math-ops.keyboard :as keyboard]))
 
 (when config/debug?
   (println "dev mode"))
+
+(defn on-key-press [event]
+  (re-frame/dispatch
+    [:press-key (keyboard/key-code event)]))
 
 (defn mount-root []
   (reagent/render [views/main-panel]
                   (.getElementById js/document "app")))
 
-(defn on-key-press [event]
-  (re-frame/dispatch [:press-key (.-charCode event)]))
-
 (defn listen-key-press []
-  (.addEventListener js/document "keypress" on-key-press))
+  (.addEventListener
+    js/document "keypress" on-key-press))
 
 (defn read-operation [o]
   (let [values (cljs.reader/read-string o)]
--- src/cljs/math_ops/keyboard.cljs
+++ src/cljs/math_ops/keyboard.cljs
@@ -2,3 +2,9 @@
 
 (defn return-pressed? [key-code]
   (= 13 key-code))
+
+(defn key-code [event]
+  (let [key-code (.-keyCode event)]
+    (if (zero? key-code)
+      (.-charCode event)
+      key-code)))
